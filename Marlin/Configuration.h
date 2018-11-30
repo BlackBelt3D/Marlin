fc9e753bf065603d56836b622e64048ae0f666b8
fieldOfView <aldo@fieldofview.com>
2018-11-30 12:17:32 +0100

    Adjust jerk & acceleration defaults
    Fixes #5

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -634,7 +634,7 @@
  * Override with M201
  *                                      X, Y, Z, E0 [, E1[, E2[, E3[, E4]]]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 500, 500, 500, 500 }
+#define DEFAULT_MAX_ACCELERATION      { 1000, 1000, 500, 500 }
 
 /**
  * Default Acceleration (change/s) change = mm/s
@@ -656,8 +656,8 @@
  * When changing speed and direction, if the difference is less than the
  * value set here, it may happen instantaneously.
  */
-#define DEFAULT_XJERK                 10.0
-#define DEFAULT_YJERK                 10.0
+#define DEFAULT_XJERK                 15.0
+#define DEFAULT_YJERK                 15.0
 #define DEFAULT_ZJERK                  1.0
 #define DEFAULT_EJERK                  8.0
 
